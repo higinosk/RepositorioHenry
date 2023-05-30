95149313bc7cf44f5aa75f7c661b5aef23f2d35b
Kamilly Higino <76918008+higinosk@users.noreply.github.com>
2023-05-30 00:23:22 -0300

    Delete M02-terminal-git-y-github directory

--- M02-terminal-git-y-github/nomenclatura.js
+++
@@ -1,5 +0,0 @@
-//Reglas principales de nomenclatura: camelCase, PascalCase, snake_case.
-
-//camelCase -> holaMundo | funcionEdadAños
-//PascalCase -> HolaMundo | FUncionEdadAños
-// snake_case -> hola_mundo | funcion_edad_años
