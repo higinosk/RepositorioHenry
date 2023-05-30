496b81d04a76d7a7509543239c9abd6763170afb
Kamilly Higino <kamilly.higinos@gmail.com>
2023-05-29 23:47:06 -0300

    MO3-JavaScript-fundamentos Clases

---
+++ M02-terminal-git-y-github/objetoMath.js
@@ -0,0 +1,20 @@
+// Objetos Math tienen varios métodos propios: Math.pow | Math.round & Math.floor & Math.cell | Math.max & Math.min | Math.random
+
+// Math.pow -> ponteciar un numero
+console.log(Math.pow(2, 3));
+
+//Math.round -> redondeará el número decimal al entero más próximo
+console.log(Math.round(5.93));
+
+//Math.floor -> redondeará el número decimal al entero de menor valor
+console.log(Math.floor(5.93));
+
+//Math.ceil -> este método, al contrario que el anterior, redondeará un número al siguiente entero
+console.log(Math.ceil(5.93));
+
+//Math.max & Math.min -> Estos dos métodos nos permitirán conocer el valor máximo o mínimo de un conjunto de números;
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+
+//Math.random -> Este último método nos permitirá generar un número aleatorio, decimal y puede ser cualquiera entre el 0 y el 1;
+console.log(Math.random());
